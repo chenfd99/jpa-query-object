28a35e9cc0577b79a2d02f3e5f62f83c2e826463
ChenFD <chenfd99@qq.com>
2023-12-11 10:37:08 +0800

    update

--- jpa-query-object/src/main/java/io/github/chenfd99/jpaqueryobject/base/QueryObject.java
+++ jpa-query-object/src/main/java/io/github/chenfd99/jpaqueryobject/base/QueryObject.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.Optional.ofNullable;
+
 
 /**
  * 基础查询类
@@ -18,13 +20,9 @@ public abstract class QueryObject<T> implements Specification<T> {
 
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-        try {
-            List<Predicate> predicates = toSpecWithLogicType(root, criteriaBuilder);
-            customPredicate(root, criteriaQuery, criteriaBuilder).ifPresent(predicates::add);
-            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        List<Predicate> predicates = toSpecWithLogicType(root, criteriaBuilder);
+        customPredicate(root, criteriaQuery, criteriaBuilder).ifPresent(predicates::add);
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
     }
 
     /**
@@ -49,14 +47,10 @@ public abstract class QueryObject<T> implements Specification<T> {
     }
 
 
-    protected List<Predicate> toSpecWithLogicType(Root<T> root, CriteriaBuilder cb) throws IllegalAccessException {
+    protected List<Predicate> toSpecWithLogicType(Root<T> root, CriteriaBuilder cb) {
         List<Field> fields = getAllFields();
         List<Predicate> predicates = new ArrayList<>();
         for (Field field : fields) {
-            field.setAccessible(true);
-            if (field.get(this) == null) {
-                continue;
-            }
 
             QFiled qf = field.getAnnotation(QFiled.class);
             QGroup qg = field.getAnnotation(QGroup.class);
@@ -65,7 +59,7 @@ public abstract class QueryObject<T> implements Specification<T> {
             }
 
             if (qf != null) {
-                Optional.ofNullable(createPredicate(root, cb, field, qf)).ifPresent(predicates::add);
+                ofNullable(createPredicate(root, cb, field, qf)).ifPresent(predicates::add);
             }
 
             if (qg != null && qg.type() != null && qg.value() != null && qg.value().length != 0) {
@@ -101,95 +95,104 @@ public abstract class QueryObject<T> implements Specification<T> {
     }
 
 
+    protected Object getFieldValue(Field field) {
+        boolean fieldAccessible = field.isAccessible();
+        try {
+            field.setAccessible(true);
+            return field.get(this);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } finally {
+            field.setAccessible(fieldAccessible);
+        }
+    }
+
     /**
      * 生成查询条件
      *
      * @param field 字段
      * @param qf    字段注解
      */
-    @SuppressWarnings({"rawtypes", "unchecked"})
+    @SuppressWarnings({"rawtypes"})
     protected Predicate createPredicate(Root<T> root, CriteriaBuilder cb, Field field, QFiled qf) {
-        String column = qf.name();
-        if (column == null || column.isEmpty()) {
-            column = field.getName();
+        Object fieldValue = getFieldValue(field);
+        if (fieldValue == null) {
+            return null;
         }
 
-        try {
-            Object value = field.get(this);
-            if (value == null) {
-                return null;
-            }
+        //不查询String空条件
+        if (String.class.isAssignableFrom(fieldValue.getClass()) && ((String) fieldValue).trim().isEmpty()) {
+            return null;
+        }
 
-            if (String.class.isAssignableFrom(value.getClass()) && value.equals("")) {
-                return null;
-            }
+        //是否是连接查询条件
+        Join join = createJoinFetch(root, cb, field, qf);
 
-            //是否是连接查询条件
-            Join join = createJoinFetch(root, cb, field, qf);
+        String column = ofNullable(qf.name()).orElse(field.getName());
+        Path path = ofNullable((From) join).orElse(root).get(column);
+        return getPredicateWithType(qf.value(), path, fieldValue, cb);
+    }
 
-            Path path = join != null ? join.get(column) : root.get(column);
-            switch (qf.value()) {
-                case EQUAL:
-                    return cb.equal(path, value);
-                case EQUAL_OR_NULL:
-                    return cb.or(cb.equal(path, value), cb.isNull(path));
-                case NOT_EQUAL_OR_NULL:
-                    return cb.or(cb.notEqual(path, value), cb.isNull(path));
-                case LIKE:
-                    return cb.like(path, "%" + value + "%");
-                case LIKE_START:
-                    return cb.like(path, "%" + value);
-                case LIKE_END:
-                    return cb.like(path, value + "%");
-                case GT:
-                    assert value instanceof Number;
-                    return cb.gt(path, (Number) value);
-                case LT:
-                    assert value instanceof Number;
-                    return cb.lt(path, (Number) value);
-                case GE:
-                    assert value instanceof Number;
-                    return cb.ge(path, (Number) value);
-                case LE:
-                    assert value instanceof Number;
-                    return cb.le(path, (Number) value);
-                case NOT_EQUAL:
-                    return cb.notEqual(path, value);
-                case NOT_LIKE:
-                    return cb.notLike(path, "%" + value + "%");
-                case GREATER_THAN:
-                    return cb.greaterThan(path, (Comparable) value);
-                case GREATER_THAN_OR_EQUAL:
-                    return cb.greaterThanOrEqualTo(path, (Comparable) value);
-                case LESS_THAN:
-                    return cb.lessThan(path, (Comparable) value);
-                case LESS_THAN_OR_EQUAL:
-                    return cb.lessThanOrEqualTo(path, (Comparable) value);
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    protected Predicate getPredicateWithType(QType qType, Path path, Object fieldValue, CriteriaBuilder cb) {
+        switch (qType) {
+            case EQUAL:
+                return cb.equal(path, fieldValue);
+            case EQUAL_OR_NULL:
+                return cb.or(cb.equal(path, fieldValue), cb.isNull(path));
+            case NOT_EQUAL_OR_NULL:
+                return cb.or(cb.notEqual(path, fieldValue), cb.isNull(path));
+            case LIKE:
+                return cb.like((Expression<String>) path, "%" + fieldValue + "%");
+            case LIKE_START:
+                return cb.like((Expression<String>) path, "%" + fieldValue);
+            case LIKE_END:
+                return cb.like((Expression<String>) path, fieldValue + "%");
+            case GT:
+                assert fieldValue instanceof Number;
+                return cb.gt((Expression<? extends Number>) path, (Number) fieldValue);
+            case LT:
+                assert fieldValue instanceof Number;
+                return cb.lt((Expression<? extends Number>) path, (Number) fieldValue);
+            case GE:
+                assert fieldValue instanceof Number;
+                return cb.ge((Expression<? extends Number>) path, (Number) fieldValue);
+            case LE:
+                assert fieldValue instanceof Number;
+                return cb.le((Expression<? extends Number>) path, (Number) fieldValue);
+            case NOT_EQUAL:
+                return cb.notEqual(path, fieldValue);
+            case NOT_LIKE:
+                return cb.notLike((Expression<String>) path, "%" + fieldValue + "%");
+            case GREATER_THAN:
+                return cb.greaterThan(path, (Comparable) fieldValue);
+            case GREATER_THAN_OR_EQUAL:
+                return cb.greaterThanOrEqualTo(path, (Comparable) fieldValue);
+            case LESS_THAN:
+                return cb.lessThan(path, (Comparable) fieldValue);
+            case LESS_THAN_OR_EQUAL:
+                return cb.lessThanOrEqualTo(path, (Comparable) fieldValue);
 
-                case NOT_IN:
-                case IN: {
-                    if (value instanceof Object[] && ((Object[]) value).length != 0) {
-                        CriteriaBuilder.In in = cb.in(path);
-                        Arrays.stream(((Object[]) value)).forEach(in::value);
-                        return qf.value() == QType.IN ? in : cb.not(in);
-                    }
-
-                    if (value instanceof Collection && !((Collection) value).isEmpty()) {
-                        CriteriaBuilder.In in = cb.in(path);
-                        ((Collection) value).forEach(in::value);
-                        return qf.value() == QType.IN ? in : cb.not(in);
-                    }
-
-                    return null;
+            case NOT_IN:
+            case IN: {
+                if (fieldValue instanceof Object[] && ((Object[]) fieldValue).length != 0) {
+                    CriteriaBuilder.In in = cb.in(path);
+                    Arrays.stream(((Object[]) fieldValue)).forEach(in::value);
+                    return qType == QType.IN ? in : cb.not(in);
                 }
 
-                case IS_NULL:
-                    return cb.isNull(path);
-                case NOT_NULL:
-                    return cb.isNotNull(path);
+                if (fieldValue instanceof Collection && !((Collection) fieldValue).isEmpty()) {
+                    CriteriaBuilder.In in = cb.in(path);
+                    ((Collection) fieldValue).forEach(in::value);
+                    return qType == QType.IN ? in : cb.not(in);
+                }
+
+                return null;
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            case IS_NULL:
+                return cb.isNull(path);
+            case NOT_NULL:
+                return cb.isNotNull(path);
         }
         return null;
     }
