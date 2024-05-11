3614a3c30c225b604f29752d984a9d048294376a
ChenFD <chenfd99@qq.com>
2024-05-12 00:47:27 +0800

    join测试用例

--- jpa-query-object-test/src/main/java/io/github/chenfd99/jpaqueryobjecttest/qo/UserJoinQO.java
+++ jpa-query-object-test/src/main/java/io/github/chenfd99/jpaqueryobjecttest/qo/UserJoinQO.java
@@ -9,10 +9,11 @@ import io.github.chenfd99.jpaqueryobjecttest.entity.User_;
 import lombok.*;
 
 import javax.persistence.criteria.JoinType;
+import java.math.BigDecimal;
 
 
 /**
- * 用户查询条件
+ * 此类默认join ORDERS和PURSE 两个表进行查询数据
  *
  * @author ChenFD
  */
@@ -23,24 +24,41 @@ import javax.persistence.criteria.JoinType;
 @AllArgsConstructor
 public class UserJoinQO extends QueryObject<User> {
 
-    @QField(
-            joinName = User_.PURSE,
-            joinType = JoinType.LEFT,
-            name = Purse_.USER,
-            value = QType.EQUAL,
-            forceJoin = true
-    )
-    private Long userId;
+    /**
+     * 查询的时候强制join ORDERS 和 PURSE 两个表
+     * 此字段其他仅作为声明强制join的作用
+     */
+    @QField(joinName = User_.ORDERS, joinType = JoinType.LEFT, forceJoin = true)
+    @QField(joinName = User_.PURSE, joinType = JoinType.LEFT, forceJoin = true)
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    private Long joinKeyword;
 
 
-    @QField(
-            joinName = User_.PURSE,
-            joinType = JoinType.LEFT,
-            name = Purse_.USER,
-            value = QType.EQUAL
-    )
-    private Long userId2;
+    /**
+     * 根据PURSE的用户id查询
+     */
+    @QField(joinName = User_.PURSE, joinType = JoinType.LEFT, name = Purse_.USER, value = QType.EQUAL)
+    private Long purseUserId;
 
+
+    /**
+     * 查询钱包余额大于等于此值的用户
+     */
+    @QField(joinName = User_.PURSE, joinType = JoinType.LEFT, name = Purse_.USER, value = QType.GREATER_THAN_OR_EQUAL)
+    private BigDecimal purseBalanceGE;
+
+
+    /**
+     * 根据订单号查询
+     */
+    @QField(joinName = User_.ORDERS, joinType = JoinType.LEFT)
+    private String orderNo;
+
+
+    /**
+     * 默认去重
+     */
     @Override
     public boolean distinct() {
         return true;
--- jpa-query-object-test/src/test/java/io/github/chenfd99/jpaqueryobjecttest/dao/UserDaoJoinTest.java
+++ jpa-query-object-test/src/test/java/io/github/chenfd99/jpaqueryobjecttest/dao/UserDaoJoinTest.java
@@ -2,9 +2,12 @@ package io.github.chenfd99.jpaqueryobjecttest.dao;
 
 import io.github.chenfd99.jpaqueryobjecttest.entity.User;
 import io.github.chenfd99.jpaqueryobjecttest.qo.UserJoinQO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 /**
  * @author ChenFD
@@ -13,15 +16,36 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class UserDaoJoinTest {
     @Autowired
     private UserDao userDao;
-    @Autowired
-    private OrderDao orderDao;
-
 
     @Test
-    void testJoinPurse() {
+    @DisplayName("无查询条件")
+    void testJoin() {
         User user = new User();
 
         UserJoinQO qo = new UserJoinQO();
+        userDao.findAll(qo);
+    }
+
+
+    @Test
+    @DisplayName("根据钱包用户id查询")
+    void testJoinWithPurseUserId() {
+        User user = new User();
+
+        UserJoinQO qo = new UserJoinQO();
+        qo.setPurseUserId(122L);
+        userDao.findAll(qo);
+    }
+
+
+    @Test
+    @DisplayName("其他条件")
+    void testJoinWithOther() {
+        User user = new User();
+
+        UserJoinQO qo = new UserJoinQO();
+        qo.setOrderNo("1111");
+        qo.setPurseBalanceGE(BigDecimal.valueOf(100.0));
         userDao.findAll(qo);
     }
 
