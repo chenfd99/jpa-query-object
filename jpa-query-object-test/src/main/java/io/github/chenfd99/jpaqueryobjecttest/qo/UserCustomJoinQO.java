8b6b90f5e802537c7eff0e6284561ed3a3cd7f0c
ChenFD <chenfd99@qq.com>
2024-06-11 16:39:56 +0800

    custom join

--- jpa-query-object-test/src/main/java/io/github/chenfd99/jpaqueryobjecttest/qo/UserCustomJoinQO.java
+++ jpa-query-object-test/src/main/java/io/github/chenfd99/jpaqueryobjecttest/qo/UserCustomJoinQO.java
@@ -2,13 +2,11 @@ package io.github.chenfd99.jpaqueryobjecttest.qo;
 
 import io.github.chenfd99.jpaqueryobject.annotation.QField;
 import io.github.chenfd99.jpaqueryobject.base.QueryObject;
-import io.github.chenfd99.jpaqueryobjecttest.entity.Purse_;
 import io.github.chenfd99.jpaqueryobjecttest.entity.User;
 import io.github.chenfd99.jpaqueryobjecttest.entity.User_;
 import jakarta.persistence.criteria.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -28,7 +26,7 @@ public class UserCustomJoinQO extends QueryObject<User> {
     /**
      * 用户id
      */
-    @QField(joinName = User_.PURSE, joinType = JoinType.LEFT, forceJoin = true)
+    @QField(name = User_.ID)
     private Long userId;
 
     /**
@@ -48,8 +46,9 @@ public class UserCustomJoinQO extends QueryObject<User> {
 
 
     @Override
-    protected void customJoin(Root<User> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-        root.join(User_.ORDERS, JoinType.LEFT);
-        root.join(User_.PURSE, JoinType.LEFT);
+    protected List<Predicate> customJoin(Root<User> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+        root.join(User_.PURSE);
+        return null;
     }
+
 }
